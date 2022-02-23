abf9ad203e30f6fee8853ff5f44baeab914a73e3
bernarberdikul <61005409+BernarBerdikul@users.noreply.github.com>
2022-02-24 04:19:41 +0600

    ADD test db & tests for roles & user roles

--- tests/functional/src/test_role.py
+++ tests/functional/src/test_role.py
@@ -7,7 +7,7 @@ pytestmark = pytest.mark.asyncio
 
 async def test_role_list_and_detail(make_request, access_token):
     """Check role's detail & list methods"""
-    """ List"""
+    """ List """
     response_list = await make_request(
         endpoint="/role/", http_method="get", headers=access_token
     )
@@ -45,12 +45,14 @@ async def test_role_create(make_request, access_token):
     assert test_role == response_detail_name
 
 
-# async def test_role_create(make_request, access_token):
-#     test_role: str = "test_role"
-#     response = await make_request(
-#         endpoint="/role/", http_method="post", headers=access_token,
-#         data={"name": test_role}
-#     )
-#     response_name: str = response.body.get("data")[0].get("name")
-#     assert response.status == http.HTTPStatus.CREATED
-#     assert response_name == test_role
+async def test_role_update(make_request, access_token, get_role_id):
+    updated_role: str = "updated_role"
+    response = await make_request(
+        endpoint=f"/role/{get_role_id}",
+        http_method="patch",
+        headers=access_token,
+        data={"name": updated_role},
+    )
+    response_name: str = response.body.get("data")[0].get("name")
+    assert response.status == http.HTTPStatus.OK
+    assert response_name == updated_role
---
+++ tests/functional/src/test_user_role.py
@@ -0,0 +1,28 @@
+import http
+
+import pytest
+
+pytestmark = pytest.mark.asyncio
+
+
+async def test_change_user_role(make_request, access_token, user, get_role_id):
+    """change user's role"""
+    response = await make_request(
+        endpoint="/user_role/change",
+        http_method="post",
+        headers=access_token,
+        data={"user_id": f"{user.id}", "role_id": get_role_id},
+    )
+    assert response.status == http.HTTPStatus.OK
+
+
+async def test_delete_user_role(make_request, access_token, user, get_role_id):
+    """Check role's detail & list methods"""
+    response = await make_request(
+        endpoint="/user_role/remove",
+        http_method="delete",
+        headers=access_token,
+        data={"user_id": f"{user.id}", "role_id": get_role_id},
+    )
+    assert response.status == http.HTTPStatus.BAD_REQUEST
+    assert response.body.get("description") == "You can not delete own role"
